82edf2ad70e79d152e49402f6792ec5643aaf79b
Stephen Feil <rogue@roguegaming.com.au>
2026-07-23 21:18:59 +1000

    feat: expand Rust operations workspace (23/25)

--- HighPop/Views/SettingsWindow.xaml.cs
+++ HighPop/Views/SettingsWindow.xaml.cs
@@ -9,13 +9,30 @@ public partial class SettingsWindow : System.Windows.Window
         InitializeComponent();
         DataContext = mainViewModel;
         SettingsViewControl.DataContext = vm;
+        StateChanged += (_, _) =>
+            MaximizeButton.Content = WindowState == System.Windows.WindowState.Maximized ? "❐" : "☐";
     }
 
     private void TitleBar_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
     {
-        if (e.ButtonState == System.Windows.Input.MouseButtonState.Pressed)
-            DragMove();
+        if (e.ChangedButton != System.Windows.Input.MouseButton.Left) return;
+        if (e.ClickCount == 2)
+        {
+            WindowState = WindowState == System.Windows.WindowState.Maximized
+                ? System.Windows.WindowState.Normal
+                : System.Windows.WindowState.Maximized;
+            return;
+        }
+        if (e.ButtonState == System.Windows.Input.MouseButtonState.Pressed) DragMove();
     }
+
+    private void MinimizeClick(object sender, System.Windows.RoutedEventArgs e) =>
+        WindowState = System.Windows.WindowState.Minimized;
+
+    private void MaximizeClick(object sender, System.Windows.RoutedEventArgs e) =>
+        WindowState = WindowState == System.Windows.WindowState.Maximized
+            ? System.Windows.WindowState.Normal
+            : System.Windows.WindowState.Maximized;
 
     private void CloseClick(object sender, System.Windows.RoutedEventArgs e) => Close();
 }
